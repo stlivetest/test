eaa6f13fbcf2aa83b2b5d9eb423aa183be889c2e
stlivetest <stlivekamo@gmail.com>
2024-03-06 13:45:48 +0900

    featureの確認

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $("#add").click(function () {
     var inputTodo = $("#input-todo").val();
     $("#todo-list").append("<li><input type='checkbox'>" + inputTodo + "</li>");
 });
-
+//kamo
 $(document).on("change", "input[type=checkbox]", function () {
     if($(this).is(":checked")) {
         $(this).parent().css("text-decoration", "line-through");
